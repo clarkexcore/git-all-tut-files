f7cb2a8a727f2b3aff113b3eaa0f4530760e5da4
Alex Clarke <clarkexcore@gmail.com>
2019-10-10 16:39:03 -0400

    Beginning of HBS Partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const path = require("path");
-
-console.log(__dirname);
-console.log(path.join(__dirname, '../public/'));
+const hbs = require('hbs');
 
 const app = express();
 
+//Define Paths for Express Config
 const public_directory = path.join(__dirname, '../public/');
+const views_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
+
+//Setup Handlebars Engine and views location
+app.set('view engine', 'hbs');
+app.set('views', views_path);
+hbs.registerPartials(partials_path);
+
+//Setup static directory to serve
 app.use(express.static(public_directory));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather",
+        name: "Alex Clarke"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About Moi",
+        name: "Alex Clarke"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help",
+        name: "Alex Clarke",
+        help_message: "Help me I am trapped in a demonic dimension, and I can't get out. Please for the love of God send help!!! Oh no they're coming! F@*k!"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
